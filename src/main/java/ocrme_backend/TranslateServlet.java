9fecea09d89b8f7f39873862355759aa188066cb
Iuliia Ashomok <ashomokdev@gmail.com>
2017-05-22 19:47:42 +0300

    translateservlet added

---
+++ src/main/java/ocrme_backend/TranslateServlet.java
@@ -0,0 +1,47 @@
+package ocrme_backend;
+
+import com.google.gson.Gson;
+import ocrme_backend.translate.RequestBean;
+import ocrme_backend.translate.Translator;
+import ocrme_backend.translate.TranslatorImpl;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Created by iuliia on 5/22/17.
+ * Run next to test in terminal
+ * curl -H "Content-Type: application/json" -X POST -d '{"sourceLang":"de","targetLang":"es", "sourceText":"Mit Macht kommt große Verantwortung."}' https://imagetotext-149919.appspot.com/translate
+ */
+public class TranslateServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse response) {
+
+        try {
+            BufferedReader reader = req.getReader();
+            Gson gson = new Gson();
+
+            RequestBean requestBean = gson.fromJson(reader, RequestBean.class);
+            Translator translator = new TranslatorImpl();
+            String translated = translator.translate(
+                    requestBean.getSourceLang(),
+                    requestBean.getTargetLang(),
+                    requestBean.getSourceText());
+
+                    response.getWriter().write(translated);
+                    response.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+            } catch (Exception e) {
+            try {
+                response.getWriter().write(e.getMessage());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/ocrme_backend/translate/RequestBean.java
@@ -0,0 +1,22 @@
+package ocrme_backend.translate;
+
+/**
+ * Created by iuliia on 5/22/17.
+ */
+public class RequestBean {
+    private String sourceLang;
+    private String targetLang;
+    private String sourceText;
+
+    public String getSourceLang() {
+        return sourceLang;
+    }
+
+    public String getTargetLang() {
+        return targetLang;
+    }
+
+    public String getSourceText() {
+        return sourceText;
+    }
+}
